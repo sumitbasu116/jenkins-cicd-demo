39a754b49b7151eb124a54edea03831b1275eb88
Sumit <sumitbasu116@gmail.com>
2025-08-23 23:53:27 +0530

    third commit

--- src/main/java/com/sumit/JenkinsCicdDemoApplication.java
+++ src/main/java/com/sumit/JenkinsCicdDemoApplication.java
@@ -2,10 +2,20 @@ package com.sumit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkinsCicdDemoApplication {
 
+	@GetMapping("/greetings/{name}")
+    public String greetings(@PathVariable String name) {
+       name= name.split(" ")[0];
+        return "Hello " + name + " Congratulations you have successfully completed Jenkins CI/CD demo !";
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsCicdDemoApplication.class, args);
 	}
