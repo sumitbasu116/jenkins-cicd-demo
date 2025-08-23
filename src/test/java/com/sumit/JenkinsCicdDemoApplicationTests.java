c217e95ced3cf4842e523ef31e7f880e9188736a
Sumit <sumitbasu116@gmail.com>
2025-08-24 01:10:46 +0530

    fake commit for jenikins testing

--- src/test/java/com/sumit/JenkinsCicdDemoApplicationTests.java
+++ src/test/java/com/sumit/JenkinsCicdDemoApplicationTests.java
@@ -15,11 +15,11 @@ class JenkinsCiCdApplicationTests {
 
 	@Test
 	public void testGreetingsEndpoint() throws Exception {
-		String name = "Basanta kumar hota";
+		String name = "Sumit Basu";
 
 		mockMvc.perform(MockMvcRequestBuilders.get("/greetings/{name}", name))
 				.andExpect(MockMvcResultMatchers.status().isOk())
-				.andExpect(MockMvcResultMatchers.content().string("Hello " + name.split(" ")[0] + " Congratulations you have successfully completed Jenkins CI/CD demo !"));
+				.andExpect(MockMvcResultMatchers.content().string("Hello " + name + " Congratulations you have successfully completed Jenkins CI/CD demo !"));
 	}
 
 }
