b41201e071bef245c7def7feb2ac189b2de5663f
Sumit <sumitbasu116@gmail.com>
2025-08-23 23:49:20 +0530

    second commit

---
+++ src/main/java/com/sumit/JenkinsCicdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sumit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsCicdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsCicdDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sumit/JenkinsCicdDemoApplicationTests.java
@@ -0,0 +1,25 @@
+package com.sumit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest
+class JenkinsCiCdApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void testGreetingsEndpoint() throws Exception {
+		String name = "Basanta kumar hota";
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/greetings/{name}", name))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().string("Hello " + name.split(" ")[0] + " Congratulations you have successfully completed Jenkins CI/CD demo !"));
+	}
+
+}
