7124522af93e49651bf0c4835608239898f2e914
Sumit <sumitbasu116@gmail.com>
2025-08-27 18:18:10 +0530

    custom-branch

--- src/main/java/com/sumit/JenkinsCicdDemoApplication.java
+++ src/main/java/com/sumit/JenkinsCicdDemoApplication.java
@@ -2,20 +2,26 @@ package com.sumit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class JenkinsCicdDemoApplication {
+public class JenkinsCicdDemoApplication extends SpringBootServletInitializer{
 
 	@GetMapping("/greetings/{name}")
     public String greetings(@PathVariable String name) {
        name= name.split(" ")[0];
         return "Hello " + name + " Congratulations you have successfully completed Jenkins CI/CD demo !";
     }
-	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // Point to your main Spring Boot class
+        return builder.sources(JenkinsCicdDemoApplication.class);
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsCicdDemoApplication.class, args);
 	}
